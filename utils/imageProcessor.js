a22e664ba6f9b8d310222a736c70e2fbe9703d5e
vlado-manov <vladimir.manov.bg@gmail.com>
2025-06-20 00:28:16 +0300

    additional options

--- utils/imageProcessor.js
+++ utils/imageProcessor.js
@@ -2,8 +2,8 @@ const sharp = require("sharp");
 const compressImage = async (filePath, outputPath) => {
   await sharp(filePath)
     .rotate()
-    .resize({ width: 1024 })
-    .jpeg({ quality: 70 })
+    .resize({ width: 1024, withoutEnlargement: true })
+    .jpeg({ quality: 70, progressive: true })
     .toFile(outputPath);
 };
 
