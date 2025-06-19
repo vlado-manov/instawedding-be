96c348c2238c67abf8870c08f38f41c72245bed7
vlado-manov <vladimir.manov.bg@gmail.com>
2025-06-20 00:27:33 +0300

    compressed images fix

--- utils/imageProcessor.js
+++ utils/imageProcessor.js
@@ -1,6 +1,7 @@
 const sharp = require("sharp");
 const compressImage = async (filePath, outputPath) => {
   await sharp(filePath)
+    .rotate()
     .resize({ width: 1024 })
     .jpeg({ quality: 70 })
     .toFile(outputPath);
